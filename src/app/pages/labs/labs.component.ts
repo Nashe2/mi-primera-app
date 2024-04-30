cccb4644e095421687d5fee8cc3ea4184d46f9ff
Nashe Osorio <nasheog2@gmail.com>
2024-04-29 23:50:44 -0600

    Cree eventos para el teclado con funcion keydownHandler para trae la informacion al input donde con reactividad cada que teclee algo la informacion del input ira cambiando sin necesidad de hacer enter

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -3,16 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-labs',
   templateUrl: './labs.component.html',
-  styleUrls: ['./labs.component.scss']
+  styleUrls: ['./labs.component.scss'],
 })
 export class LabsComponent {
   title = 'mi-primera-app';
   welcome = 'Bienvenido a mi primera aplicación Angular';
-  tasks = [
-    'Instalar el Angular CLI',
-    'Crear proyecto',
-    'Crear componentes'
-  ];
+  tasks = ['Instalar el Angular CLI', 'Crear proyecto', 'Crear componentes'];
   name = 'Mila';
   age = 27;
   disabled = true;
@@ -20,18 +16,26 @@ export class LabsComponent {
 
   //Declaro un objeto con sus propiedades
   person = {
-    name : 'Nashe',
+    name: 'Nashe',
     age: 18,
-    avatar: 'https://w3schools.com/howto/img_avatar.png'
-  }
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
+  };
 
   //Método de mostrar una alerta al dar click en el boton
-  clickHandler(){
-    alert('Hola')
+  clickHandler(): void {
+    alert('Hola');
   }
 
   //Método de evento, es para hacer un cambio en el input
-  changeHandler(event: Event){
+  changeHandler(event: Event): void {
     console.log(event);
+  }
+
+  //El método keydonwHandler usa ese evento para que cada vez que teclee
+  //en el teclado el evento traiga el target (la información) al input
+  //y lo cache el HTML (el input de la página donde se desea la reactividad)
+  keydownHandler(event: KeyboardEvent): void {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
   }
 }
